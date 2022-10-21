670afa8d3f3992266f3b28227b4c5ce145ddb1bd
daniela11cuellar <lcuellar@applaudostudios.dev>
2022-10-20 19:49:44 -0500

    Test case header_03

---
+++ cypress/e2e/header/header_03.cy.js
@@ -0,0 +1,46 @@
+import indexPage from "../../pages/index"; 
+import searchPage from "../../pages/search"; 
+
+var keyWord = "Blouse";
+
+describe('Verify "Sign in" page can be opened', () => {
+  
+  it('go to url', () => {
+    cy.visit('https://automationpractice.com')
+  })
+
+  it('type input search: '+keyWord, () => {
+    indexPage.typeInputSearch(keyWord);
+  })
+
+  it('click button search', () => {
+    indexPage.clickButtonSearch();
+  })
+
+  it('validate search url', () => {
+    cy.url()
+      .should('eq', 'http://automationpractice.com/index.php?controller=search&orderby=position&orderway=desc&search_query='+keyWord+'&submit_search=');
+  })
+
+  it('validate search title with keyword: '+keyWord, () => {
+    searchPage.validateTitleSearch(keyWord);
+  })
+
+  it('click list icon', () => {
+    searchPage.clickButtonList();
+  })
+
+  it('validate the image is present', () => {
+    searchPage.validateImageIsPresent();
+  })
+
+  it('validate the price is present', () => {
+    searchPage.validateThePriceIsPresent();
+  })
+
+  it('validate the showing item is present', () => {
+    searchPage.validateShowingItem();
+  })
+  
+})
+
--- cypress/pages/index.js
+++ cypress/pages/index.js
@@ -2,10 +2,24 @@ class indexPage{
 
     constructor(){
         this.btnSignIn = ".login"; 
+        this.searchInput = "#search_query_top";
+        this.btnSearch = ".button-search";   
+        this.btnList = ".icon-th-list";
     }
 
     clickBtnSignIn = () =>{
         cy.get(this.btnSignIn)
+            .click();
+    }
+
+    typeInputSearch = (keyWord) =>{
+        cy.get(this.searchInput)
+            .click()
+            .type(keyWord);
+    }
+
+    clickButtonSearch = () =>{
+        cy.get(this.btnSearch)
             .click();
     }
 }
