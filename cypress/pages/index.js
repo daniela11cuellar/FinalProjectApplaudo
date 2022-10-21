10e16164f939f0cb25523ae0bf8564b68d35dd2e
daniela11cuellar <lcuellar@applaudostudios.dev>
2022-10-21 12:15:42 -0500

    Test case header_04

--- cypress/pages/index.js
+++ cypress/pages/index.js
@@ -1,13 +1,23 @@
-class indexPage{
+class IndexPage{
 
     constructor(){
-        this.btnSignIn = ".login"; 
+        this.btnSignIn = ".login";
+        this.categories = ".sf-menu";
     }
 
     clickBtnSignIn = () =>{
         cy.get(this.btnSignIn)
             .click();
     }
+
+    clickOnCategory = (category) =>{
+        cy.get(this.categories)
+        .within(() => {
+            return cy.get('[title="'+category+'"]')
+            .first().click();
+          })
+    }
+    
 }
 
-module.exports = new indexPage();
+module.exports = new IndexPage();
