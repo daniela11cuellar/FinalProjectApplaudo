b928000a94f904bf0fd7046510bd2d857d4fea70
Carlos Rafael Contreras <112312360+ccontrerasapplaudo@users.noreply.github.com>
2022-10-24 01:22:53 -0600

    Test case product_listing_01

--- cypress/pages/ProductListing.js
+++ cypress/pages/ProductListing.js
@@ -1,7 +1,8 @@
 class ProductListingFeature {
     constructor() {
-        this.productList = "ul.product_list";
-        this.productListElements = "ul.product_list li.ajax_block_product";
+        this.productList = "ul.product_list"
+        this.productListElements = "ul.product_list li.ajax_block_product"
+        this.noProductWarning = "p.alert-warning"
     }
 
     verifyIfListHasElements = () => {
@@ -14,10 +15,8 @@ class ProductListingFeature {
         cy.get(this.productList).should("not.exist")
     }
 
-    verifyIfListHasNoElements = () => {
-        cy.get(this.productList).should("be.visible").then(() => {
-            cy.get(this.productListElements).should("have.length", 0)
-        })
+    verifyIfWarningMessageExists = () => {
+        cy.get(this.noProductWarning).should("be.visible").and("have.css","background-color","rgb(254, 145, 38)").and("have.css","border-color","rgb(228, 117, 43)")
     }
 
 }
