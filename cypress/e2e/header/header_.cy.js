5bf71bb09fd7d30353271ed63c7062e9c418bfa1
daniela11cuellar <lcuellar@applaudostudios.dev>
2022-10-24 20:08:38 -0500

    Use strict and id test cases

--- cypress/e2e/header/header_.cy.js
+++ cypress/e2e/header/header_.cy.js
@@ -1,3 +1,4 @@
+"use strict";
 import IndexPage from "../../pages/Index"; 
 import SearchPage from "../../pages/Search"; 
 import CategoryPage from "../../pages/Category"; 
@@ -11,18 +12,21 @@ describe('validate the header section can be navigate', () => {
     cy.visit('https://automationpractice.com');
   })
   
+  //HEADER_01
   it('Sign in can be opened', () => {
     IndexPage.clickBtnSignIn();
     cy.url()
     .should('eq', 'http://automationpractice.com/index.php?controller=authentication&back=my-account');
   })
 
+  //HEADER_02
   it('shopping cart can be viewed', () => {
     IndexPage.clickBtnCart();
     cy.url()
     .should('eq', 'http://automationpractice.com/index.php?controller=order');
   })
 
+  //HEADER_03
   it('user is able to search clothes from the header', () => {
     IndexPage.typeInputSearch(keyWord);
     IndexPage.clickButtonSearch();
@@ -35,6 +39,7 @@ describe('validate the header section can be navigate', () => {
     SearchPage.validateShowingItem();
   })
 
+  //HEADER_04
   it('navigation menu of "Women, Dresses , T-Shirts', () => {
     IndexPage.clickOnCategory(category);
     CategoryPage.validateTitleCategory(category);
