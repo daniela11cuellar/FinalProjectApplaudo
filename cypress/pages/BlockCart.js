dfc99ace4e47fcc9bf1b7c9b89b4af5dbadcc156
Manuel Garcia <116128392+mgarciaapplaudo@users.noreply.github.com>
2022-10-25 20:41:10 -0400

    feat: Add test cases for Block Cart

---
+++ cypress/pages/BlockCart.js
@@ -0,0 +1,35 @@
+"use strict";
+
+class BlockCart {
+
+    constructor() {
+        this.divCart = "div#layer_cart";
+        this.btnProceedToCheckout = "div#layer_cart div.button-container a[title*='checkout']";
+        this.btnContinueShopping = "div#layer_cart div.button-container span[title*='shopping']";
+        this.btnCloseWindow = "div#layer_cart span.cross";
+        this.headingResultMessage = "h2";
+    }
+
+    getDivCart(){
+        return cy.get(this.divCart);
+    }
+
+    getBtnProceedToCheckout(){
+        return cy.get(this.btnProceedToCheckout);
+    }
+
+    getBtnContinueShopping(){
+        return cy.get(this.btnContinueShopping);
+    }
+
+    getBtnCloseWindow(){
+        return cy.get(this.btnCloseWindow);
+    }
+
+    getHeadingResultMessage(){
+        return cy.get(this.headingResultMessage).contains("added to your shopping cart");
+    }
+
+}
+
+module.exports = BlockCart;
