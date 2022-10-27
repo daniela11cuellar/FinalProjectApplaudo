4384b9ba24fcc0ff10dc26e7d552c0ca3179f5a9
daniela11cuellar <lcuellar@applaudostudios.dev>
2022-10-27 10:31:19 -0500

    Change api test

--- cypress/e2e/API/apiTest.cy.js
+++ cypress/e2e/API/apiTest.cy.js
@@ -1,17 +1,10 @@
 const{dataContactUs} = require('../../fixtures/dataContactUs.json');
 const url = "http://automationpractice.com/index.php?controller=contact";
-const url_cypress = "https://jsonplaceholder.cypress.io/posts";
 
 describe('TODO api testing', () => {
 
     it(' POST - Contact us', () => {
         cy.request('POST', url, dataContactUs).then(($response)=>{
-            expect(201).to.eq($response.status)
-        })
-    });
-
-    it(' POST - Cypress', () => {
-        cy.request('POST', url_cypress, dataContactUs).then(($response)=>{
             expect(201).to.eq($response.status)
         })
     });
