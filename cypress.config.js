940f721599822fead4f33ce7ca35150f5bbe119c
daniela11cuellar <lcuellar@applaudostudios.dev>
2022-10-26 21:26:16 -0500

    Api test and reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,27 +1,28 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       on('before:browser:launch', (browser = {}, launchOptions) => {
-        if (browser.family === 'chromium' && browser.name !== 'electron' && browser.name !== 'edge') {
-          launchOptions.args.push("--incognito");
-          return launchOptions
-        }
+              if (browser.family === 'chromium' && browser.name !== 'electron' && browser.name !== 'edge') {
+                launchOptions.args.push("--incognito");
+                return launchOptions
+              }
 
-        if (browser.name === 'electron') {
-          launchOptions.preferences.incognito = true
-          return launchOptions
-        }
+              if (browser.name === 'electron') {
+                launchOptions.preferences.incognito = true
+                return launchOptions
+              }
 
-        if (browser.name === 'edge') {
-          launchOptions.args.push("-inprivate");
-          return launchOptions
-        }
-      })
+              if (browser.name === 'edge') {
+                launchOptions.args.push("-inprivate");
+                return launchOptions
+              }
+            })
     },
-    baseUrl: "http://automationpractice.com/index.php",
+    baseUrl: "http://automationpractice.com/index.php"
   },
 });
-
