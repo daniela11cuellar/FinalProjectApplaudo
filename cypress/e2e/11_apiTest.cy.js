35ff87306bab30f9d0d225280801adab0375f81a
Carlos Rafael Contreras <112312360+ccontrerasapplaudo@users.noreply.github.com>
2022-11-04 18:07:43 -0600

    refactor: Refactor Api Test

--- cypress/e2e/11_apiTest.cy.js
+++ cypress/e2e/11_apiTest.cy.js
@@ -1,12 +1,14 @@
-const{dataContactUs} = require('../fixtures/dataContactUs.json');
-const url = "http://automationpractice.com/index.php?controller=contact";
+"use strict";
 
-describe('TODO api testing', () => {
+const url_post = "http://automationpractice.com/index.php?controller=contact";
+const url_get = "https://mocki.io/v1/f81f6929-4523-4680-bbb2-01fa03407be0";
 
-    it(' POST - Contact us', () => {
-        cy.request('POST', url, dataContactUs).then(($response)=>{
-            expect(201).to.eq($response.status)
-        })
+describe('Api testing', () => {
+    it(' GET and POST - Contact us', () => {
+        cy.request('GET',url_get).then(($response_get) =>{
+            cy.request('POST', url_post, $response_get.body).then(($response_post)=>{
+                expect(200).to.eq($response_post.status)
+            });
+        });
     });
-
  });
